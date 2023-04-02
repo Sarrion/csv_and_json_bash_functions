bbd01d9f1884af8013ccf6fc13be9223729af3f0
sarrion <sarrionmas.a@gmail.com>
2023-04-02 20:39:51 +0200

    added csv generation function

--- json_csv_bash_functions.sh
+++ json_csv_bash_functions.sh
@@ -83,3 +83,53 @@ function sarrion.filter {
 function sarrion.csvpprint {
     sed 's/,,/, ,/g' | awk 'BEGIN {FS=","}{print} NR==1 {gsub(/[^,]/, "-"); print}' | column -s, -t
 }
+
+
+
+generate_csv() {
+    # Check that two arguments were passed
+    if [ $# -lt 2 ]; then
+        echo "Usage: generate_csv <field_1_name>,<field_1_type> [<field_2_name>,<field_2_type> ...] <number_of_rows>"
+        return 1
+    fi
+
+
+
+  # print header
+  local header=""
+  for pair in "${@:1:$#-1}"; do
+    fst="${pair%,*}"
+    header+="${fst},"
+  done
+  echo "${header::-1}"
+
+  # Generate random data for each field
+  last_arg="${@: -1}"
+  for ((i=0; i<$last_arg; i++)); do
+    local row=""
+    # Generate random values for each field based on the field type
+    for pair in "${@:1:$#-1}"; do
+        type="${pair#*,}"
+        case $type in
+            int)
+                val="$((RANDOM % 100))"
+                ;;
+            string)
+                val="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)"
+                ;;
+            float)
+                val="$(awk -v min=0 -v max=1 'BEGIN{srand(); print min+rand()*(max-min)}' | tr ',' '.')"
+                ;;
+            date)
+                val="$(date -d "$((RANDOM % 30)) days ago" +%Y%m%d)"
+                ;;
+            datetime)
+                val="$(date -d "$((RANDOM % 30)) days ago $((RANDOM % 24)) hours ago $((RANDOM % 60)) minutes ago" +"%Y%m%d %H:%M:%S")"
+                ;;
+        esac
+        row+="${val},"
+    done
+    echo "${row::-1}" 
+  done
+}
+
